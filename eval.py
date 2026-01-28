81f7b4918f628663f245064941eb223ca29148b2
PraveenBushipaka <praveenbushipaka942@gmail.com>
2026-01-28 22:38:57 +0100

    Refactor evaluation script for data handling and metrics

--- eval.py
+++ eval.py
@@ -1,48 +1,65 @@
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0' 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0' # can run only one GPU :(
 
 print("right now using device num:", os.environ['CUDA_VISIBLE_DEVICES'])
 import pandas as pd
-from eval_utils import compute_mu_scores, compute_fe_scores
+from eval_utils import compute_mu_scores, compute_fq_scores
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from config import Config
+from config import Config_eval
 from peft import PeftModel
-from utils import update_json_dict, coerce_to_list
+from utils import update_json_dict
 from template import LLAMA3_CHAT_TEMPLATE
 import warnings
 from transformers import logging as hf_logging
 from tabulate import tabulate
 from sentence_transformers import SentenceTransformer
+import json
 
 hf_logging.set_verbosity_error()
 
 warnings.filterwarnings("ignore")
 
-cfg = Config()
+cfg = Config_eval()
 print('loading forget and test set')
 
-forget = pd.read_csv(cfg.forget_path, converters={'perturbed_answers': coerce_to_list})
-retain_data = pd.read_csv(cfg.retain_full)
+
+def read_file(path):
+    if path.endswith('.csv'):
+        df = pd.read_csv(path)
+    elif path.endswith('.json'):
+        df = pd.read_json(path)
+    elif path.endswith('.parquet'):
+        df = pd.read_parquet(path)
+    return df
+
+forget = read_file(cfg.forget_path)
+
+
+test_data = pd.read_csv(cfg.test_path, encoding="latin1")
 
 device = 'cuda'
 
+print('forget shape:', forget.shape)
+print('\ntest shape:', test_data.shape)
 
 # ---- Loading Tokenizer -----------
 tokenizer = AutoTokenizer.from_pretrained(cfg.model_id)
-tokenizer.pad_token = '<|finetune_right_pad_id|>'
+tokenizer.pad_token = tokenizer.eos_token #'<|finetune_right_pad_id|>'
 
 
 
 print(f'\n\nConducting evaluation on: {cfg.loss_type}_{cfg.exp_type}')
+
+
 
 # ---- Loading model -----------
 if cfg.loss_type == 'pre_unlearning':
      model = AutoModelForCausalLM.from_pretrained(cfg.model_id, token = cfg.access_token, device_map = "auto", torch_dtype=torch.bfloat16)
 else:
      print('loading peft model from ', cfg.save_dir)
-     base_model = AutoModelForCausalLM.from_pretrained(cfg.model_id, token = cfg.access_token, device_map = "auto", torch_dtype=torch.bfloat16)
+     base_model = AutoModelForCausalLM.from_pretrained(cfg.model_id, token = cfg.access_token, device_map = "auto", torch_dtype=torch.bfloat16) #attn_implementation='flash_attention_2'
      model = PeftModel.from_pretrained(base_model, cfg.save_dir, device_map="auto", torch_dtype=torch.bfloat16) 
      model = model.merge_and_unload()
 
@@ -53,8 +70,8 @@ def make_template_format(df):
      return df
 
 forget_2 = make_template_format(forget)
-retain_data = make_template_format(retain_data)
-forget_2 = forget_2[['title','question', 'answer', 'perturbed_answers','paraphrased_question', 'paraphrased_answer', 'num_tokens']]
+test_data = make_template_format(test_data)
+forget_2 = forget_2[['id','question', 'answer', 'perturbed_answers','paraphrased_question', 'paraphrased_answer', 'num_tokens', 'type', 'source']]
 print(forget_2.columns)
 
 
@@ -64,23 +81,25 @@ embedding_model = SentenceTransformer(model_name, device=device)
 
 print('\ncalculating forget efficacy')
 
-forget,all_scores_fe, fe, f_ppl  = compute_fe_scores(df=forget_2, model=model, tokenizer=tokenizer, embedding_model=embedding_model, device=device)
-print('\nforget efficacy', fe)
+forget,all_scores_fe, fq, f_ppl  = compute_fq_scores(df=forget_2, model=model, tokenizer=tokenizer, embedding_model=embedding_model, device=device)
+print('\nforget quality', fq)
 
 
 print('\ncalculating model utility')
 
-retain,all_scores_mu, mu, rt_ppl  = compute_mu_scores(df=retain_data, model=model, tokenizer=tokenizer, embedding_model=embedding_model, device=device)
+retain,all_scores_mu, mu, rt_ppl  = compute_mu_scores(df=test_data, model=model, tokenizer=tokenizer, embedding_model=embedding_model, device=device)
 
-forget['type'] = 'forget'
 retain['perturbed_answers'] = None
 retain['truth'] = None
 
 df = pd.concat([forget, retain], axis=0)
-df.to_csv(f'./results/datasets/{cfg.loss_type}_{cfg.exp_type}.csv', index = False) 
+df.to_csv(f'./results/datasets/{cfg.loss_type}_{cfg.exp_type}_{cfg.data_type}.csv', index = False) 
+
+
 
 metrics = [
-    ("FE",      fe.item()),
+     ("experiment_type", f'{cfg.loss_type}_{cfg.exp_type}_{cfg.data_type}'),
+    ("FQ",      fq.item()),
     ("MU",   mu.item()),
     ("PPL-F",   f_ppl.item()),
     ("PPL-R",rt_ppl.item()),
@@ -102,8 +121,8 @@ except ImportError:
         print(f"| {name.ljust(col1_w)} | {val:>{col2_w}.4f} |")
 
 
-results = {f'{cfg.loss_type}_{cfg.exp_type}': 
-            {"FE":      fe.item(),
+results = {f'{cfg.loss_type}_{cfg.loss_type}_{cfg.exp_type}': 
+            {"FQ":      fq.item(),
             "MU":   mu.item(),
             'forget_scores' : all_scores_fe.tolist(),
             'retain_scores': all_scores_mu.tolist(),
@@ -118,4 +137,4 @@ results = {f'{cfg.loss_type}_{cfg.exp_type}':
             'LoRA_alpha': cfg.LoRA_alpha,
             }}
 
-update_json_dict(f'./results/scores/{cfg.loss_type}_{cfg.exp_type}_results.json', results)
+update_json_dict(f'./results/scores/{cfg.loss_type}_{cfg.exp_type}_{cfg.data_type}_results.json', results)
